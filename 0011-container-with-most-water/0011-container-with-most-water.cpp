3ce6e446016764e7659ff85fb7a4178186a4cf12
Mousam Podder <mousampodder02@gmail.com>
2026-04-08 19:43:46 +0530

    Time: 4 ms (21.48%), Space: 63 MB (47.79%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,23 @@
+#include<algorithm>
+class Solution {
+public:
+    int area(auto i,auto j){
+        return (j-i)* min(*i,*j);
+    }
+    int maxArea(vector<int>& height) {
+        auto i=height.begin();
+        auto j=height.end()-1;
+        int result=0;
+
+        while(i<j)
+        {
+            result=max(result, area(i,j));
+            if(*i<*j){
+                i++;
+            }else {
+                j--;
+            }
+           
+        }  return result;
+    }
+};
