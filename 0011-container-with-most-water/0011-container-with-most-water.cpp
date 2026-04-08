a7978bc771d22f4fd8edd4e41583c6d8a75ce9d1
Mousam Podder <mousampodder02@gmail.com>
2026-04-08 21:19:58 +0530

    Time: 4 ms (21.48%), Space: 63 MB (47.79%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -6,13 +6,11 @@ public:
         int result = 0;
 
         while (i < j) {
-            // Calculate area directly to save function call time
+        
             int h = (height[i] < height[j]) ? height[i] : height[j];
             int current_area = (j - i) * h;
-            
             if (current_area > result) result = current_area;
 
-            // Move the shorter pointer
             if (height[i] < height[j]) {
                 i++;
             } else {
